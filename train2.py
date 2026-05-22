d8bc2728b66d9e43f03c2936edf86556ca4bfa54
sabrina <sabrina080802@hotmail.fr>
2026-05-22 21:29:26 +0200

    add move img and 3 training

--- train2.py
+++ train2.py
@@ -1,9 +1,31 @@
 from ultralytics import YOLO
+import torch
 
+device = "mps" if torch.backends.mps.is_available() else "cpu"
+print(f"Device utilisé : {device}")
+
+# On repart de ton meilleur modèle actuel
 model = YOLO("runs/detect/runs/train/hands_yolo11n-2/weights/best.pt")
 
 model.train(
     data="hands.yolo26/data.yaml",
-    epochs=20,
-    device="mps"
+    epochs=40,
+    imgsz=640,
+    batch=4,
+    device=device,
+    workers=0,
+
+    # Amélioration dataset / généralisation
+    degrees=10,
+    translate=0.15,
+    scale=0.6,
+    shear=2,
+    perspective=0.0005,
+    fliplr=0.5,
+    hsv_h=0.015,
+    hsv_s=0.6,
+    hsv_v=0.4,
+
+    # Nom clair
+    name="hands_boost_perf",
 )
